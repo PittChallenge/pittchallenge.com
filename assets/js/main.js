92d994a501b073a1b38975a2b7fa42ecf92713f7
Ritwik Gupta <ritwikg2004@live.com>
2017-01-11 22:13:40 -0500

    Add Reis video

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,163 +1,166 @@
 /*
-	Stellar by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+    Stellar by HTML5 UP
+    html5up.net | @ajlkn
+    Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
 (function($) {
 
-	skel.breakpoints({
-		xlarge: '(max-width: 1680px)',
-		large: '(max-width: 1280px)',
-		medium: '(max-width: 980px)',
-		small: '(max-width: 736px)',
-		xsmall: '(max-width: 480px)',
-		xxsmall: '(max-width: 360px)'
-	});
+    skel.breakpoints({
+        xlarge: '(max-width: 1680px)',
+        large: '(max-width: 1280px)',
+        medium: '(max-width: 980px)',
+        small: '(max-width: 736px)',
+        xsmall: '(max-width: 480px)',
+        xxsmall: '(max-width: 360px)'
+    });
 
-	$(function() {
+    $(function() {
 
-		var	$window = $(window),
-			$body = $('body'),
-			$main = $('#main');
+        var $window = $(window),
+            $body = $('body'),
+            $main = $('#main');
 
-		// Disable animations/transitions until the page has loaded.
-			$body.addClass('is-loading');
+        // Disable animations/transitions until the page has loaded.
+            $body.addClass('is-loading');
 
-			$window.on('load', function() {
-				window.setTimeout(function() {
-					$body.removeClass('is-loading');
-				}, 100);
-			});
+            $window.on('load', function() {
+                window.setTimeout(function() {
+                    $body.removeClass('is-loading');
+                }, 100);
+            });
 
-		// Fix: Placeholder polyfill.
-			$('form').placeholder();
+        // Fix: Placeholder polyfill.
+            $('form').placeholder();
 
-		// Prioritize "important" elements on medium.
-			skel.on('+medium -medium', function() {
-				$.prioritize(
-					'.important\\28 medium\\29',
-					skel.breakpoint('medium').active
-				);
-			});
+        // Prioritize "important" elements on medium.
+            skel.on('+medium -medium', function() {
+                $.prioritize(
+                    '.important\\28 medium\\29',
+                    skel.breakpoint('medium').active
+                );
+            });
 
-		// Nav.
-			var $nav = $('#nav');
+        // Nav.
+            var $nav = $('#nav');
 
-			if ($nav.length > 0) {
+            if ($nav.length > 0) {
 
-				// Shrink effect.
-					$main
-						.scrollex({
-							mode: 'top',
-							enter: function() {
-								$nav.addClass('alt');
-							},
-							leave: function() {
-								$nav.removeClass('alt');
-							},
-						});
+                // Shrink effect.
+                    $main
+                        .scrollex({
+                            mode: 'top',
+                            enter: function() {
+                                $nav.addClass('alt');
+                            },
+                            leave: function() {
+                                $nav.removeClass('alt');
+                            },
+                        });
 
-				// Links.
-					var $nav_a = $nav.find('a');
+                // Links.
+                    var $nav_a = $nav.find('a');
 
-					$nav_a
-						.scrolly({
-							speed: 1000,
-							offset: function() { return $nav.height(); }
-						})
-						.on('click', function() {
+                    $nav_a
+                        .scrolly({
+                            speed: 1000,
+                            offset: function() { return $nav.height(); }
+                        })
+                        .on('click', function() {
 
-							var $this = $(this);
+                            var $this = $(this);
 
-							// External link? Bail.
-								if ($this.attr('href').charAt(0) != '#')
-									return;
+                            // External link? Bail.
+                                if ($this.attr('href').charAt(0) != '#')
+                                    return;
 
-							// Deactivate all links.
-								$nav_a
-									.removeClass('active')
-									.removeClass('active-locked');
+                            // Deactivate all links.
+                                $nav_a
+                                    .removeClass('active')
+                                    .removeClass('active-locked');
 
-							// Activate link *and* lock it (so Scrollex doesn't try to activate other links as we're scrolling to this one's section).
-								$this
-									.addClass('active')
-									.addClass('active-locked');
+                            // Activate link *and* lock it (so Scrollex doesn't try to activate other links as we're scrolling to this one's section).
+                                $this
+                                    .addClass('active')
+                                    .addClass('active-locked');
 
-						})
-						.each(function() {
+                        })
+                        .each(function() {
 
-							var	$this = $(this),
-								id = $this.attr('href'),
-								$section = $(id);
+                            var $this = $(this),
+                                id = $this.attr('href'),
+                                $section = $(id);
 
-							// No section for this link? Bail.
-								if ($section.length < 1)
-									return;
+                            // No section for this link? Bail.
+                                if ($section.length < 1)
+                                    return;
 
-							// Scrollex.
-								$section.scrollex({
-									mode: 'middle',
-									initialize: function() {
+                            // Scrollex.
+                                $section.scrollex({
+                                    mode: 'middle',
+                                    initialize: function() {
 
-										// Deactivate section.
-											if (skel.canUse('transition'))
-												$section.addClass('inactive');
+                                        // Deactivate section.
+                                            if (skel.canUse('transition'))
+                                                $section.addClass('inactive');
 
-									},
-									enter: function() {
+                                    },
+                                    enter: function() {
 
-										// Activate section.
-											$section.removeClass('inactive');
+                                        // Activate section.
+                                            $section.removeClass('inactive');
 
-										// No locked links? Deactivate all links and activate this section's one.
-											if ($nav_a.filter('.active-locked').length == 0) {
+                                        // No locked links? Deactivate all links and activate this section's one.
+                                            if ($nav_a.filter('.active-locked').length == 0) {
 
-												$nav_a.removeClass('active');
-												$this.addClass('active');
+                                                $nav_a.removeClass('active');
+                                                $this.addClass('active');
 
-											}
+                                            }
 
-										// Otherwise, if this section's link is the one that's locked, unlock it.
-											else if ($this.hasClass('active-locked'))
-												$this.removeClass('active-locked');
+                                        // Otherwise, if this section's link is the one that's locked, unlock it.
+                                            else if ($this.hasClass('active-locked'))
+                                                $this.removeClass('active-locked');
 
-									}
-								});
+                                    }
+                                });
 
-						});
+                        });
 
-			}
+            }
 
-		// Scrolly.
-			$('.scrolly').scrolly({
-				speed: 1000
-			});
+        // Scrolly.
+            $('.scrolly').scrolly({
+                speed: 1000
+            });
 
-	});
+    });
 
 })(jQuery);
 
 function vidSwitch(num) {
-	var frame = document.getElementById('challenge-vids'); 
-	switch (num) {
-		case 'one':
-			frame.src = "https://www.youtube.com/embed/TESgXLwI8DU?list=PLIi-JVODZkTPptRTj5_Xh-yykA5cttBVl";
-			break;
-		case 'two':
-			frame.src = "https://www.youtube.com/embed/mv2RytgqNQU?list=PLIi-JVODZkTPptRTj5_Xh-yykA5cttBVl";
-			break;
-		case 'three':
-			frame.src = "https://www.youtube.com/embed/X6QBq6H5g00?list=PLIi-JVODZkTPptRTj5_Xh-yykA5cttBVl";
-			break; 
-		case 'four':
-			frame.src = "https://www.youtube.com/embed/2q87tGGafsk?list=PLIi-JVODZkTPptRTj5_Xh-yykA5cttBVl";
-			break;
-		case 'five':
-			frame.src = "https://www.youtube.com/embed/irW938cqClY?list=PLIi-JVODZkTPptRTj5_Xh-yykA5cttBVl";
-			break;
-		default:
-			frame.src = "https://www.youtube.com/embed/TESgXLwI8DU?list=PLIi-JVODZkTPptRTj5_Xh-yykA5cttBVl";
-			break;
-	}
+    var frame = document.getElementById('challenge-vids');
+    switch (num) {
+        case 'one':
+            frame.src = "https://www.youtube.com/embed/TESgXLwI8DU?list=PLIi-JVODZkTPptRTj5_Xh-yykA5cttBVl";
+            break;
+        case 'two':
+            frame.src = "https://www.youtube.com/embed/mv2RytgqNQU?list=PLIi-JVODZkTPptRTj5_Xh-yykA5cttBVl";
+            break;
+        case 'three':
+            frame.src = "https://www.youtube.com/embed/X6QBq6H5g00?list=PLIi-JVODZkTPptRTj5_Xh-yykA5cttBVl";
+            break;
+        case 'four':
+            frame.src = "https://www.youtube.com/embed/2q87tGGafsk?list=PLIi-JVODZkTPptRTj5_Xh-yykA5cttBVl";
+            break;
+        case 'five':
+            frame.src = "https://www.youtube.com/embed/irW938cqClY?list=PLIi-JVODZkTPptRTj5_Xh-yykA5cttBVl";
+            break;
+        case 'six':
+            frame.src = "https://www.youtube.com/embed/_I4ba25LJ0k?list=PLIi-JVODZkTPptRTj5_Xh-yykA5cttBVl";
+            break;
+        default:
+            frame.src = "https://www.youtube.com/embed/TESgXLwI8DU?list=PLIi-JVODZkTPptRTj5_Xh-yykA5cttBVl";
+            break;
+    }
 }
