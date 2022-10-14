ebe408a10be14f5e7d5f589dab0caca73b89432d
Vivswan Shah <58091053+Vivswan@users.noreply.github.com>
2022-10-14 19:41:52 -0400

    image size fix

--- z_js/proportional-image.js
+++ z_js/proportional-image.js
@@ -1,16 +1,15 @@
-(function () {
-    "use strict";
-    function adjustImageWidth(image) {
-        const widthBase = 70;
-        const scaleFactor = 1.2;
-        const imageRatio = image.naturalWidth / image.naturalHeight;
+"use strict";
+function adjustImageWidth(image) {
+    const scaleFactor = 1.2;
+    const imageRatio = image.naturalWidth / image.naturalHeight;
+    image.width = imageRatio * scaleFactor * window.innerWidth * 0.05;
+}
 
-        image.width = Math.pow(imageRatio, scaleFactor) * widthBase;
-    }
-
-    window.onload = function () {
-        const images = document.querySelectorAll("img.attendees-images");
-        images.forEach(adjustImageWidth);
-    };
-
-}());
+window.addEventListener("resize", function () {
+    const images = document.querySelectorAll("img.attendees-images");
+    images.forEach(adjustImageWidth);
+})
+window.addEventListener("load", function () {
+    const images = document.querySelectorAll("img.attendees-images");
+    images.forEach(adjustImageWidth);
+})
