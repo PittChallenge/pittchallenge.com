aa3ca810de1d87dfa4bd40754b1652a3307c7fb6
Vivswan Shah <58091053+Vivswan@users.noreply.github.com>
2022-10-15 13:55:01 -0400

    sponsors logo size fix

--- z_js/proportional-image.js
+++ z_js/proportional-image.js
@@ -1,9 +1,8 @@
 "use strict";
 function adjustImageWidth(image) {
-    const aspectRatio = image.naturalWidth / image.naturalHeight;
-    image.style.height = "100px";
-    image.style.maxWidth = (document.querySelector("#attendees").clientWidth - 32 * 2) + "px";
+    let dataSize = 1
+    if (image.hasAttribute("data-size")) dataSize = image.getAttribute("data-size")
+    image.style.maxHeight = (100 * dataSize) + "px";
 }
 
-window.addEventListener("resize", () => document.querySelectorAll("img.attendees-images").forEach(adjustImageWidth))
 window.addEventListener("load", () => document.querySelectorAll("img.attendees-images").forEach(adjustImageWidth))
