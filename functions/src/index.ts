c10a9078a7c43130de69e48fc01d47489d24952d
Vivswan Shah <58091053+Vivswan@users.noreply.github.com>
2023-07-15 19:43:31 -0400

    getCSV

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -65,3 +65,98 @@ export const addRegistrationEntry = onRequest({
         response.status(500).send("Internal server error");
     });
 });
+
+// export const fillRandomData = onRequest({
+//     cors: true,
+//     timeoutSeconds: 60,
+//     cpu: 1,
+//     invoker: "public",
+// }, (request, response) => {
+//     const db = getFirestore();
+//     const promises = [];
+//     for (let i = 0; i < 20; i++) {
+//         const id = Math.floor(Math.random() * 1000000);
+//         const email = id + "@example.com";
+//         const timestamp = new Date().toISOString();
+//         const randomName = Math.random().toString(36).substring(2, 15);
+//         let json: any = {
+//             id,
+//             email,
+//             timestamp,
+//         };
+//         json[randomName] = randomName;
+//         promises.push(db.collection("registrations_id").doc(id.toString()).set(json));
+//         promises.push(db.collection("registrations_email").doc(email).set(json));
+//     }
+//
+//     Promise.all(promises).then(() => {
+//         response.status(200).send("OK");
+//     }).catch((error) => {
+//         logger.error(error);
+//         response.status(500).send("Internal server error");
+//     });
+// });
+
+export const getCSV = onRequest({
+    cors: true,
+    timeoutSeconds: 60,
+    cpu: 1,
+    invoker: "public",
+}, (request, response) => {
+    if (request.method !== "GET") {
+        response.status(400).send("Invalid request");
+        return;
+    }
+
+    const readApiKey = defineString("READ_API_KEY");
+    if (request.query.key !== readApiKey.value()) {
+        response.status(401).send("Unauthorized");
+        return;
+    }
+
+    if (request.query.type !== "id" && request.query.type !== "email") {
+        response.status(400).send("Invalid query parameter");
+        return;
+    }
+
+    const collectionType = request.query.type === "id" ? "id" : "email";
+    const collectionName = "registrations_" + collectionType;
+    const db = getFirestore();
+    const registrations = db.collection(collectionName).get().then((snapshot) => {
+        const registrations: any[] = [];
+        snapshot.forEach((doc) => {
+            registrations.push(doc.data());
+        });
+        return registrations;
+    });
+
+    // find all the headers
+    const headers = registrations.then((registrations) => {
+        const headers = new Set<string>();
+        registrations.forEach((registration) => {
+            Object.keys(registration).forEach((key) => {
+                headers.add(key);
+            });
+        });
+        return headers;
+    });
+
+    // generate the CSV
+    Promise.all([registrations, headers]).then(([registrations, headers]) => {
+        const csv = [
+            Array.from(headers).join(","),
+            ...registrations.map((registration) => {
+                return Array.from(headers).map((header) => {
+                    return registration[header] ?? "";
+                }).join(",");
+            }),
+        ].join("\n");
+
+        response.set("Content-Type", "text/csv");
+        response.set("Content-Disposition", "attachment; filename=" + collectionName + ".csv");
+        response.status(200).send(csv);
+    }).catch((error) => {
+        logger.error(error);
+        response.status(500).send("Internal server error");
+    });
+});
